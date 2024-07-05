4ad5ff08c95d719786175fb5f81f57fe809b08a6
GilbertoVM <gvalenzuela@ucol.mx>
2024-07-05 16:58:00 -0600

    adaptacion de usuario y responsive

--- src/components/mainComponents/topBar.vue
+++ src/components/mainComponents/topBar.vue
@@ -59,7 +59,7 @@
       <!-- se muestran los links en computadoras -->
       <ul class="linksContainer">
         <div class="dropdown" @mouseover="showMenu" @mouseleave="scheduleHideMenu">
-          <a to="/userProfile" class="dropdown-button link">
+          <a to="/user" class="dropdown-button link">
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-square svgs"
               viewBox="0 0 16 16">
               <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
@@ -68,12 +68,14 @@
             </svg> {{ username }}</a>
           <div class="dropdown-menu" :class="{ visible: isMenuVisible }" @mouseover="cancelHideMenu"
             @mouseleave="scheduleHideMenu">
-            <router-link to="/userProfile" class="dropdown-item">
+            <router-link to="/user" class="dropdown-item">
               <i class="bi bi-person-square"></i> Profile</router-link>
-            <router-link to="/userProfile/my-orders" class="dropdown-item">
+            <router-link to="/user" class="dropdown-item">
               <i class="bi bi-bag"></i> My Orders</router-link>
-            <router-link to="/userProfile/sold-items" class="dropdown-item">
-              <i class="bi bi-shop"></i> Sold Items</router-link>
+            <router-link to="/user" class="dropdown-item">
+              <i class="bi bi-trophy"></i> Rewards</router-link>
+            <router-link to="/user" class="dropdown-item">
+              <i class="bi bi-universal-access-circle"></i> Accessibility</router-link>
             <a @click="handleLogout" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Log Out</a>
           </div>
         </div>
@@ -82,11 +84,12 @@
             <path
               d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
           </svg> Shopping Car</router-link>
-        <a to="/" class="link"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-star svgs"
-            viewBox="0 0 16 16">
+        <router-link to="/user" class="link"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+            class="bi bi-star svgs" viewBox="0 0 16 16">
             <path
               d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z" />
-          </svg> Favorites</a>
+          </svg> Favorites</router-link>
+
       </ul>
     </template>
     <template v-else>
--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,39 @@
 import Vue from "vue";
 import Router from "vue-router";
-import store from "../store/store.js"
-
+import store from "../store/store.js";
+// MAIN VIEWS
 import NotFoundPage from "../views/mainViews/notFoundPage.vue";
 import homePage from "../views/mainViews/homePage.vue";
-import bestSellers from "../views/mainViews/bestSellersPage.vue";
-import newItems from "../views/mainViews/newItemsPage.vue";
-import moreEcofriendly from "../views/mainViews/moreEcofriendlyPage.vue";
-import shoppingCar from "../views/userViews/shoppingCarPage.vue"
-import userProfile from "../views/userViews/userProfilePage.vue";
-import signIn from "../views/LoginViews/signInPage.vue"
-import signUp from "../views/LoginViews/signUpPage.vue"
-// ROUTES ADMIN
-import indexAdmin from "../views/adminViews/index.vue"
+import signInPage from "../views/LoginViews/signInPage.vue";
+import signUpPage from "../views/LoginViews/signUpPage.vue";
+import bestSellersPage from "../views/mainViews/bestSellersPage.vue";
+import newItemsPage from "../views/mainViews/newItemsPage.vue";
+import moreEcofriendlyPage from "../views/mainViews/moreEcofriendlyPage.vue";
+import shoppingCarPage from "../views/userViews/shoppingCarPage.vue";
+import outletPage from "../views/mainViews/outletPage.vue";
+import promosPage from "../views/mainViews/promosPage.vue";
+// ADMIN VIEWS
+// import indexAdminPage from "../views/adminViews/index.vue";
+// USER VIEWS
+import userProfilePage from "../views/userViews/userProfilePage.vue";
+
 
 Vue.use(Router);
 
 const router = new Router({
   mode: 'history',
   routes: [
-    {path:'/', name: 'home', component: homePage},
-    {path:'/best-sellers', name: 'bestSellers', component: bestSellers},
-    {path:'/new-items', name: 'newItems', component: newItems},
-    {path:'/more-ecofriendly', name: 'moreEcofriendly', component: moreEcofriendly},
-    {path: '/shopping-car',name: 'shoppingCar',component: shoppingCar, meta: {requiresAuth:true}},
-    {path: '/userProfile', name: 'userProfile', component: userProfile, meta: {requiresAuth:true}},
-    {path: '/sign-in',name: 'signIn', component: signIn, meta:{requiresGuest: true}},
-    {path: '/sign-up',name: 'signUn', component: signUp, meta:{requiresGuest: true}},
-    {path: '/admin/index', name: 'indexAdmin', component: indexAdmin, meta: {requiresAuth:false}},
+    {path: '/', name: 'home', component: homePage},
+    {path: '/sign-in',name: 'signInPage', component: signInPage, meta:{requiresGuest: true}},
+    {path: '/sign-up',name: 'signUpPage', component: signUpPage, meta:{requiresGuest: true}},
+    {path: '/outlet', name: 'outletPage', component: outletPage},
+    {path: '/promotions', name: 'promosPage',component: promosPage},
+    {path: '/best-sellers', name: 'bestSellersPage', component: bestSellersPage},
+    {path: '/new-items', name: 'newItemPages', component: newItemsPage},
+    {path: '/more-ecofriendly', name: 'moreEcofriendlyPage', component: moreEcofriendlyPage},
+    {path: '/shopping-car',name: 'shoppingCarPage',component: shoppingCarPage, meta: {requiresAuth:true}},
+    {path: '/user', name: 'userProfilePage', component: userProfilePage, meta: {requiresAuth:true}},
+    // {path: '/admin/index', name: 'indexAdminPage', component: indexAdminPage, meta: {requiresAuth:false}},
     {path: '*', name: '404NotFound',component: NotFoundPage},
 ]
 })
